e53c7032acf4267f14e10df9ebebbcbcc429d080
oweissbarth <mail@oweissbarth.de>
2017-05-02 22:22:45 +0200

    force string or double type

--- src/main/scala/de/oweissbarth/sample/CSVSampleProvider.scala
+++ src/main/scala/de/oweissbarth/sample/CSVSampleProvider.scala
@@ -3,7 +3,11 @@ package de.oweissbarth.sample
 import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.SparkContext
 
+import org.apache.spark.sql.types.{StructType, StructField, StringType, DoubleType}
+
 import scala.util.Try
+
+import scala.io.Source
 
 /** supplies a Sample as read from a csv file
   *
@@ -20,7 +24,18 @@ class CSVSampleProvider(filepath :String, delimiter: String ) extends SampleProv
     val sc = SparkContext.getOrCreate()
     val sqlc = SQLContext.getOrCreate(sc)
 
-    val file = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("delimiter", delimiter).option("inferSchema", "true").load(filepath)
+    val src = Source.fromFile(filepath).getLines()
+    val labels = src.next().split(delimiter)
+    var first = src.next().split(delimiter)
+
+    val schema = labels zip first map({case (l :String, e:String )=> if(Try(e.toDouble).isSuccess){
+                                                                          StructField(l, DoubleType, true)
+                                                                    }else{
+                                                                          StructField(l, StringType, true)
+                                                                    }})
+    val fullSchema = StructType(schema)
+
+    val file = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("delimiter", delimiter).schema(fullSchema).load(filepath)
     
     new Sample(file)
 
